bb3b1b160230aa69238c4844e4db34c0b7a84173
Kyle Fan <kmfan28@gmail.com>
2026-02-07 13:39:13 -0800

    chore: image-related bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
+import VolunteerApplication from "@/components/volunteerApplication";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <h1>Home</h1>
+      <VolunteerApplication />
     </main>
   );
 }
