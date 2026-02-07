45fc677e26e8956d343d7163fb7e6c865779c2b6
Kyle Fan <kmfan28@gmail.com>
2026-02-07 13:31:58 -0800

    Revert "feat: add form styling"
    This reverts commit 47406ee76d637c6db7ac3cc3d00cf77e9474f2f7.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/Navbar";
-import VolunteerApplication from "@/components/volunteerApplication";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <h1>Home</h1>
-      <VolunteerApplication />
     </main>
   );
 }
