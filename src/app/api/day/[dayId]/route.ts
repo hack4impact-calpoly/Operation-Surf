b4098b32e10ee89cad3590725b9472de5d159e9f
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2026-03-01 22:33:03 -0800

    feat: get day by params

---
+++ src/app/api/day/[dayId]/route.ts
@@ -0,0 +1,51 @@
+import connectDB from "@/database/db";
+import { NextResponse } from "next/server";
+import Day from "@/database/models/daySchema";
+
+/**
+ * gets 1 day from the database based on the dayId
+ * returns the day in the database as a JSON response
+ * if an error occurs, returns a JSON response with an error message and status code 500
+ */
+
+type IParams = {
+  params: {
+    dayId: string;
+  };
+};
+
+export async function GET(request: Request, { params }: IParams): Promise<NextResponse> {
+  // Attempt to connect to the database
+  await connectDB();
+
+  const { dayId } = params;
+
+  try {
+    const day = await Day.findOne({ dayId: dayId });
+    // check if day exists
+    if (!day) {
+      return NextResponse.json(
+        {
+          message: "Day not found.",
+        },
+        { status: 404 },
+      );
+    }
+    // if day exists, return it
+    return NextResponse.json(
+      {
+        day: day,
+      },
+      { status: 200 },
+    );
+  } catch (err) {
+    console.error("Error fetching day:", err);
+    return NextResponse.json(
+      {
+        message: "Failed to fetch program.",
+        error: err instanceof Error ? err.message : "An unknown error occurred.",
+      },
+      { status: 500 },
+    );
+  }
+}
