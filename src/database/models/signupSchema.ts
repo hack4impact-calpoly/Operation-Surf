020107185276a8b74f70ac1282eaec92e65d1112
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2026-03-11 20:31:18 -0700

    fix: timestamp as a Date object

--- src/database/models/signupSchema.ts
+++ src/database/models/signupSchema.ts
@@ -3,20 +3,22 @@ import mongoose, { Schema } from "mongoose";
 /* 
 figma shows that liability form is yes/no
 so used a boolean for the waiver field. If it's true, the user has signed the waiver; if it's false, they haven't
+
+Using Date for timestamp because it will automatically store the date and time when the signup is created, which can be useful for tracking when users signed up for shifts.
  */
 
 type signup = {
   shiftId: String;
   profileId: String;
   waiver: Boolean;
-  timestamp: String;
+  timestamp: Date;
 };
 
 const signupSchema = new Schema<signup>({
   shiftId: { type: String, required: true },
   profileId: { type: String, required: true },
   waiver: { type: Boolean, required: true },
-  timestamp: { type: String, required: true },
+  timestamp: { type: Date, required: true },
 });
 
 const Signup = mongoose.models["signup"] || mongoose.model("signup", signupSchema, "signup");
