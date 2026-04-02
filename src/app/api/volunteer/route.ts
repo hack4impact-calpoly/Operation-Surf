b86afaa703440e58cdb448c6a0171d783b9b9cdc
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2026-04-02 13:42:47 -0700

    fix: create user with better auth, then create user data

--- src/app/api/volunteer/route.ts
+++ src/app/api/volunteer/route.ts
@@ -2,6 +2,7 @@ import connectDB from "@/database/db";
 import bcrypt from "bcrypt";
 import Volunteer from "@/database/models/volunteerSchema";
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/lib/auth";
 
 const SALT_ROUNDS = 10;
 
@@ -26,6 +27,8 @@ export async function GET(request: NextRequest) {
 
 /* 
 Frontend should enforce validation of required fields such as email format, password strength, and valid shirt sizes before sending the request. The backend will check for the presence of required fields but will rely on the frontend for detailed validation.
+
+Better Auth signin first to validate username and get userId
 
 Note: height = number in cm
       sex = one of "female", "male", "intersex", "prefer_not_to_say", "other"
@@ -80,37 +83,26 @@ export async function POST(request: NextRequest) {
 
     const body = await request.json();
 
-    // required fields validation
-    const required = [
-      "name",
-      "username",
-      "password",
-      "email",
-      "phone",
-      "height",
-      "weight",
-      "sex",
-      "birthday",
-      "location",
-      "emergencyContact",
-      "skillsOrExperience",
-      "shirtSize",
-      "interests",
-      "liabilityWaiver",
-    ];
+    //create user documents
 
-    const missing = required.filter((key) => !Object.prototype.hasOwnProperty.call(body, key));
-    if (missing.length) {
-      return NextResponse.json({ error: `Missing required volunteer fields: ${missing.join(", ")}` }, { status: 400 });
+    const data = await auth.api.signUpEmail({
+      body: {
+        name: body.name,
+        username: body.username,
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    console.log(data);
+
+    if (!data) {
+      throw new Error("username is taken");
     }
 
-    // hash password before saving
-
-    const hashedPassword = await bcrypt.hash(body.password, SALT_ROUNDS);
-
     const volunteerData = {
+      userId: data.user.id,
       ...body,
-      password: hashedPassword,
     };
 
     const volunteer = new Volunteer(volunteerData);
@@ -118,13 +110,12 @@ export async function POST(request: NextRequest) {
 
     return NextResponse.json(volunteer, { status: 201 });
   } catch (err) {
-    console.error("Error fetching signups:", err);
     return NextResponse.json(
       {
         message: "Failed to create volunteer.",
         error: err instanceof Error ? err.message : "An unknown error occurred.",
       },
-      { status: 500 },
+      { status: 400 },
     );
   }
 }
