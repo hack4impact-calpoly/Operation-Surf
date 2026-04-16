f380f3c879ddb940a3fb922e8bbd6ee731ec9aa5
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2026-04-16 12:24:34 -0700

    fix: added description

--- src/database/models/Shift.ts
+++ src/database/models/Shift.ts
@@ -12,6 +12,7 @@ const ShiftSchema = new Schema(
     shiftId: { type: String, required: true, unique: true, trim: true },
     eventId: { type: String, required: true, trim: true },
     invited: { type: [String], required: true }, // array of volunteerIds to check if someone is invited to the shift
+    description: { type: String, required: true, trim: true },
   },
   {
     timestamps: true,
