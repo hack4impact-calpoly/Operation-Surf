4f9bfbaefdaafc4a8f4b10f683793d6fbe8d89c5
BrianLi808 <brian.li.social@gmail.com>
2026-04-19 13:30:13 -0700

    Fix: return not-found for private or ghost programs when unathenticated

--- src/app/api/program/[programId]/route.ts
+++ src/app/api/program/[programId]/route.ts
@@ -1,6 +1,7 @@
 import connectDB from "@/database/db";
 import { NextResponse } from "next/server";
 import Program from "@/database/models/programSchema";
+import { getAuthContext } from "@/lib/authz";
 
 /**
  * gets 1 program from the database based on the programId
@@ -17,6 +18,7 @@ type IParams = {
 export async function GET(request: Request, { params }: IParams): Promise<NextResponse> {
   // Attempt to connect to the database
   await connectDB();
+  const authContext = await getAuthContext();
 
   const { programId } = params;
 
@@ -31,6 +33,16 @@ export async function GET(request: Request, { params }: IParams): Promise<NextRe
         { status: 404 },
       );
     }
+
+    if ((program.private || program.ghost_program) && !authContext.isAuthenticated) {
+      return NextResponse.json(
+        {
+          message: "Program not found.",
+        },
+        { status: 404 },
+      );
+    }
+
     // if program exists, return it
     return NextResponse.json(
       {
